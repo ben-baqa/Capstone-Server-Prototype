7d2ce0a2614d74935932c2c50553b1d2462a6826
Benjamin Nielsen <56326234+ben-baqa@users.noreply.github.com>
2022-01-06 17:29:28 -0700

    Updated socket handler to allow for multiple simultaneous requests

--- socketHandler.js
+++ socketHandler.js
@@ -2,6 +2,7 @@ const {database} = require('./databaseManager')
 const WebSocket = require('ws')
 const https = require('https')
 const fs = require('fs')
+const { request } = require('express')
 
 let sockets = {}
 let httpsServer
@@ -21,7 +22,7 @@ exports.createSocketServer = (port) => {
     wsServer = new WebSocket.Server({ server: httpsServer })
 
     wsServer.on('connection', (ws) => {
-        ws.on('message', handleMessage)
+        ws.on('message', parseMessage)
         ws.on('close', () => {
             for (var key in sockets)
                 if (sockets[key] == ws) delete sockets[key]
@@ -43,13 +44,18 @@ let getValidID = () => {
     return n
 }
 
-// handles incoming messages with appropriate respones to sender
-let handleMessage = (m) => {
-    m = m.toString()
-    console.log(m)
-    m = m.split(":")
-    let ar = m[0].split("/")
-    let id = parseInt(m[1])
+// parses raw message to allow execution of multiple commands
+let parseMessage = (message) => {
+    console.log(message.toString())
+    let [requests, id] = message.toString().split(':')
+    requests.split("&").forEach(request => {
+        handleRequest(request, id)
+    });
+}
+
+// handles incoming requests with appropriate respones to sender
+let handleRequest = (request, id) => {
+    let segments = request.split("/")
 
     if(id < 0 || id >= sockets.length)
     {
@@ -57,30 +63,30 @@ let handleMessage = (m) => {
         return;
     }
 
-    console.log(`From Socket ${id}: `, m[0])
+    // console.log(`From Socket ${id}: `, request)
 
-    switch (ar[0]) {
+    switch (segments[0]) {
         case "get": // get information
             // format: get/...:id
-            handleGet(ar.slice(1), id)
+            handleGet(segments.slice(1), id)
             break
 
         case "post":{ // add message
             // format: post/sender/text/channel:id
-            const [sender, text, channel] = ar.slice(1)
+            const [sender, text, channel] = segments.slice(1)
             console.log('from: ' + sender + "\tin channel " + channel + '\n' + text)
             database.add(sender, text, channel)}
             break
         
         case "put":{ // modify message
             // format: put/sender/date/newText:id
-            const [sender, date, newText] = ar.slice(1)
+            const [sender, date, newText] = segments.slice(1)
             database.modify(sender, date, newText)}
             break
 
         case "delete":{ // modify message
             // format: delete/sender/date:id
-            const [sender, date] = ar.slice(1)
+            const [sender, date] = segments.slice(1)
             database.delete(sender, date)}
             break
 
@@ -96,8 +102,8 @@ let handleMessage = (m) => {
 }
 
 // handles information requests
-let handleGet = async(ar, id) => {
-    switch (ar[0]) {
+let handleGet = async(segments, id) => {
+    switch (segments[0]) {
         case 'all': // get all messages
             // format: get/all:id
             let all = await database.getAll()
@@ -112,7 +118,7 @@ let handleGet = async(ar, id) => {
 
         case 'channel': // get all messages in channel
             // format get/channel/channelId:id
-            let messages = await database.getMessagesInChannel(ar[1])
+            let messages = await database.getMessagesInChannel(segments[1])
             sockets[id].send(messages)
             break
     
